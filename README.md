8cd721712e662f41d60b13fb0da37469881a3313
Forrest Oliphant <forrest@sembiki.com>
2016-08-06 23:06:16 +0300

    edit starting to work

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ed-userhtml
 Ed widget for user html
+
+Widget: edit.html and src/edit.js -- zips html into url of runner
+
+Runner: index.html and src/index.js -- unzips html from url and mounts arbitrary html
+
+## dev
+
+    npm run devedit
+
+or
+
+    npm run devindex
+
+& open http://localhost:9966/demo.html
+
+## deploy
+
+    npm version (patch|minor|major)
--- src/edit.js
+++ src/edit.js
@@ -1,45 +1,73 @@
 var deflate = require('pako/lib/deflate').deflate
 var encode = require('urlsafe-base64').encode
+var RUNNER = 'https://the-grid.github.io/ed-userhtml/?g='
+
+var cachedBlock = null
+var textarea = document.getElementById('textarea')
+var status = document.getElementById('status')
+
+var cachedHeight = 0
 
 function b64ify (text) {
   var zipped = deflate(text, {to: 'string'})
   var ascii = window.btoa(zipped)
   var b64 = encode(ascii)
   return b64
-} 
+}
 
-console.log('edit', deflate)
+function send (topic, payload) {
+  if (!cachedBlock || !cachedBlock.id) return
+  window.parent.postMessage(
+    { topic: topic
+    , id: cachedBlock.id
+    , payload: payload
+    }
+  , '*'
+  )
+}
 
-var block = {}
-var userhtml = document.getElementById('userhtml')
-var status = document.getElementById('status')
-
-function toEdit () {
+function toEdit (block) {
+  cachedBlock = block
   if (block && block.hasOwnProperty('metadata') && block.metadata.hasOwnProperty('text')) {
-    userhtml.value = block.metadata.text
+    textarea.value = block.metadata.text
   }
+  resize()
 }
 
 function fromEdit (event) {
+  if (!cachedBlock || !cachedBlock.metadata) return
+
   var text = event.target.value
   var b64 = b64ify(text)
 
-  block.metadata.text = text
-  block.html = b64
+  cachedBlock.metadata.text = text
+  cachedBlock.html = RUNNER + b64
 
-  // status.textContent = 'Changed...'
-  console.log(block)
+  send('changed', cachedBlock)
+  resize()
 }
-userhtml.addEventListener('input', fromEdit)
+textarea.addEventListener('input', fromEdit)
+
+function resize () {
+  textarea.style.height = 'auto'
+  var height = textarea.scrollHeight
+  textarea.style.height = (height - 4) + 'px'
+  console.log(height, cachedHeight)
+  if (height !== cachedHeight) {
+    cachedHeight = height
+    send('height', height)
+  }
+}
 
 window.addEventListener('message', function (message) {
   switch (message.data.topic) {
     case 'setblock':
-      block = message.data.payload
-      toEdit()
+      var block = message.data.payload
+      toEdit(block)
       break
     case 'focus':
-      userhtml.focus()
+      textarea.focus()
       break
   }
 })
+
