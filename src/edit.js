9e35f43d5bdbf878e5bdcfe14236285f5763923a
Forrest Oliphant <forrest@sembiki.com>
2016-08-07 11:54:22 +0300

    edit: status & too long check #1

--- src/edit.js
+++ src/edit.js
@@ -1,12 +1,25 @@
 var deflate = require('pako/lib/deflate').deflate
 var encode = require('urlsafe-base64').encode
 var RUNNER = 'https://the-grid.github.io/ed-userhtml/?g='
+var MAX_URL_LENGTH = 2000
 
 var cachedBlock = null
 var textarea = document.getElementById('textarea')
 var status = document.getElementById('status')
-
 var cachedHeight = 0
+var hideStatusTimeout = null
+
+function showStatus (text) {
+  if (hideStatusTimeout) {
+    window.clearTimeout(hideStatusTimeout)
+    hideStatusTimeout = null
+  }
+  status.textContent = text
+}
+
+function hideStatus () {
+  status.textContent = ''
+}
 
 function b64ify (text) {
   var zipped = deflate(text, {to: 'string'})
@@ -37,13 +50,21 @@ function toEdit (block) {
 function fromEdit (event) {
   if (!cachedBlock || !cachedBlock.metadata) return
 
+  showStatus('saving...')
+
   var text = event.target.value
   var b64 = b64ify(text)
 
   cachedBlock.metadata.text = text
   var url = RUNNER + b64
+  if (url.length > MAX_URL_LENGTH) {
+    showStatus('error: input too long')
+    return
+  }
   cachedBlock.metadata.isBasedOnUrl = url
   cachedBlock.html = '<iframe src="'+url+'"></iframe>'
+
+  hideStatusTimeout = setTimeout(hideStatus, 1000)
 
   send('changed', cachedBlock)
   resize()
